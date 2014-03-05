0cb942316fbeb3aa2fa5a1f2793134c0e973871c
tomsteele <thomasjsteele@gmail.com>
2014-03-05 10:32:22 -0800

    Adds test for validation of custom names

--- csrf_test.go
+++ csrf_test.go
@@ -248,3 +248,65 @@ func Test_Validate(t *testing.T) {
 		t.Error("Validation of X-CSRFToken failed")
 	}
 }
+func Test_ValidateCustom(t *testing.T) {
+	m := martini.Classic()
+	store := sessions.NewCookieStore([]byte("secret123"))
+	m.Use(sessions.Sessions("my_session", store))
+	m.Use(Generate(&Options{
+		Secret:     "token123",
+		SessionKey: "userId",
+		Header:     "X-SEESurfToken",
+		Form:       "_seesurf",
+	}))
+
+	// Simulate login.
+	m.Get("/login", func(s sessions.Session) string {
+		s.Set("userId", "123456")
+		return "OK"
+	})
+
+	// Generate token.
+	m.Get("/private", func(s sessions.Session, x Csrf) string {
+		return x.GetToken()
+	})
+
+	m.Post("/private", Validate, func(s sessions.Session) string {
+		return "OK"
+	})
+
+	// Login to set session.
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/login", nil)
+	m.ServeHTTP(res, req)
+
+	cookie := res.Header().Get("Set-Cookie")
+
+	// Get a new token.
+	res2 := httptest.NewRecorder()
+	req2, _ := http.NewRequest("GET", "/private", nil)
+	req2.Header.Set("Cookie", cookie)
+	m.ServeHTTP(res2, req2)
+
+	// Post using custom form value.
+	data := url.Values{}
+	data.Set("_seesurf", res2.Body.String())
+	res3 := httptest.NewRecorder()
+	req3, _ := http.NewRequest("POST", "/private", bytes.NewBufferString(data.Encode()))
+	req3.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req3.Header.Set("Content-Length", strconv.Itoa(len(data.Encode())))
+	req3.Header.Set("Cookie", cookie)
+	m.ServeHTTP(res3, req3)
+	if res3.Code == 400 {
+		t.Error("Valiation of _seesurf custom form value failed")
+	}
+
+	// Post using custom HTTP header.
+	res4 := httptest.NewRecorder()
+	req4, _ := http.NewRequest("POST", "/private", nil)
+	req4.Header.Set("X-SEESurfToken", res2.Body.String())
+	req4.Header.Set("Cookie", cookie)
+	m.ServeHTTP(res4, req4)
+	if res4.Code == 400 {
+		t.Error("Validation of X-SEESurfToken custom header value failed")
+	}
+}
